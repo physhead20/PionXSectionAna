b0f9e41ba110732ac033351c1586e4cce5c26f72
Jonathan <physhead20@gmail.com>
2016-06-07 14:20:42 -0500

    Adding Proton MC Energy Corrections

--- ProtonMCAna/ProtonEnergyCorrections.C
+++ ProtonMCAna/ProtonEnergyCorrections.C
@@ -12,7 +12,25 @@
 TH1D *hMCELossUpstream = new TH1D("hMCELossUpstream", "Energy loss prior to entering the TPC", 1000, 0, 1000);
 
 /////////////////////////////////// Energy Loss in the TPC  ///////////////////////
-TH1D *hMCELossInTPC = new TH1D("hMCELossInTPC", "Energy loss inside the TPC", 1000, 0, 1000);
+TH1D *hMCELossInTPC = new TH1D("hMCELossInTPC", "Energy loss inside the TPC", 110, -100, 1000);
+
+/////////////////////////////////// Initial Kinetic Energy /////////////////////////
+TH1D *hInitialKE = new TH1D("hInitialKE", "Initial Kinetic Energy", 110, -100, 1000);
+
+/////////////////////////////////// Initial Kinetic Energy @ the TPC /////////////////////////
+TH1D *hInitialKEAtTPC = new TH1D("hInitialKEAtTPC", "Initial Kinetic Energy at the TPC", 110, -100, 1000);
+
+/////////////////////////////////// Final Kinetic Energy in the TPC /////////////////////////
+TH1D *hFinalKEInTPC = new TH1D("hFinalKEInTPC", "Final Kinetic Energy in the TPC", 110, -100, 1000);
+
+/////////////////////////////////// E Loss upstream of the TPC /////////////////////////
+TH2D *hELossXvsY = new TH2D("hELossXvsY", "Energy Loss X vs Y", 86, 0, 43, 80, -20, 20);
+
+/////////////////////////////////// E Loss upstream of the Flux TPC /////////////////////////
+TH2D *hELossXvsYFlux = new TH2D("hELossXvsYFlux", "Energy Loss X vs Y", 86, 0, 43, 80, -20, 20);
+
+/////////////////////////////////// Divided E Loss /////////////////////////
+TH2D *hELossXvsYDivide = new TH2D("hELossXvsYDivide", "Energy Loss X vs Y", 86, 0, 43, 80, -20, 20);
 
 
 void ProtonEnergyCorrections::Loop()
@@ -43,8 +61,12 @@ void ProtonEnergyCorrections::Loop()
 // ##########################################################
 // ### Putting in some counters for event reduction table ###
 // ##########################################################
-int nTotalEvents = 0;
+int nTotalEvents = 0, nEventsReachTPC = 0, nParticlesStop = 0;
 
+
+
+bool EventsReachTPC = false;
+bool EventsWhereParticleStops = false;
 
 float particle_mass = 938.28 ;
 
@@ -82,6 +104,12 @@ for (Long64_t jentry=0; jentry<nentries;jentry++)
    
    // === Outputting every nEvents to the screen ===
    if(nTotalEvents % 1000 == 0){std::cout<<"Event = "<<nTotalEvents<<std::endl;} 
+   
+   // ### Assume the event doesn't reach the TPC ###
+   EventsReachTPC = false;
+   
+   // ### Assume the particle does stop ###
+   EventsWhereParticleStops = true;
       
    //=======================================================================================================================
    //					      GEANT 4 Information
@@ -95,23 +123,47 @@ for (Long64_t jentry=0; jentry<nentries;jentry++)
       EnergyLossOutsideTPC = 0;
       EnergyLossInsideTPC = 0;
       
+      
+      float FinalKEInTPC = 0;
+      
       // #########################################################
       // ### Skipping any particle which doesn't enter the TPC ###
       // #########################################################
       if(EndPointz[iG4] < 0){continue;}
       
+      // ### If we are here, the event must pass through the TPC ###
+      EventsReachTPC = true;
+      
+      // #######################################################
+      // ### Skipping the event if the particle doesn't stop ###
+      // #######################################################
+      if(EndPointz[iG4] < 0 || EndPointz[iG4] > 90 || EndPointx[iG4] < 0 || 
+         EndPointx[iG4] > 43 || EndPointy[iG4] < -20 || 
+	 EndPointy[iG4] > 20 || NumberDaughters[iG4] > 0) {EventsWhereParticleStops = false; continue; }
+      
+      
+      float g4Primary_ProjX0 = 0.0;
+      float g4Primary_ProjY0 = 0.0;
+      float g4Primary_ProjZ0 = 0.0;      
       // #############################################
       // ### Loop over all the primary true points ###
       // #############################################
-      if(process_primary[iG4] == 1)
+      if(process_primary[iG4] == 1 && NumberDaughters[iG4] == 0)
          {
-	 //std::cout<<"NTrTrajPts[iG4] = "<<NTrTrajPts[iG4]<<std::endl;
+	 
+	 // ############################################
+	 // ### Calculate the initial Kinetic Energy ###
+	 // ############################################
+	 
+	 float momentum = sqrt( (Px[iG4] * Px[iG4]) + (Py[iG4] * Py[iG4]) + (Pz[iG4] * Pz[iG4]) ) * 1000;
+	 
+	 float InitialKE = sqrt( (momentum*momentum) + (particle_mass*particle_mass)) - particle_mass;
+	 
+	 hInitialKE->Fill(InitialKE);
+	 
 	 for(int iPriTrj = 1; iPriTrj < NTrTrajPts[iG4]; iPriTrj++)
 	    {
-	    
-	    
-	    //if(iPriTrj > 999){continue;}
-	    
+	    	    
 	    // ### Only looking at points which are upstream of the TPC ###
 	    if(MidPosZ[iG4][iPriTrj] < 0)
 	       {
@@ -132,7 +184,7 @@ for (Long64_t jentry=0; jentry<nentries;jentry++)
 	       }//<---End Looking at energy loss upstream of TPC
 	       
 	    // ### Only looking at points which are inside of the TPC ###
-	    if(MidPosZ[iG4][iPriTrj] > 0 && MidPosZ[iG4][iPriTrj] < 90 &&
+	    if(MidPosZ[iG4][iPriTrj] >= 0 && MidPosZ[iG4][iPriTrj] < 90 &&
 	       MidPosX[iG4][iPriTrj] > 0 && MidPosX[iG4][iPriTrj] < 43 &&
 	       MidPosY[iG4][iPriTrj] > -20 && MidPosY[iG4][iPriTrj] < 20 )
 	       {
@@ -163,14 +215,57 @@ for (Long64_t jentry=0; jentry<nentries;jentry++)
 	    
 	 hMCELossUpstream->Fill(EnergyLossOutsideTPC);
 	 hMCELossInTPC->Fill(EnergyLossInsideTPC);
+	 
+	 // ###############################################################
+	 // ### Calculating the initial KE at the front face of the TPC ###
+	 // ###############################################################
+	 float InitialKEAtTPC = InitialKE - EnergyLossOutsideTPC;
+	 
+	 hInitialKEAtTPC->Fill(InitialKEAtTPC);
+	 
+	 // ###############################################
+	 // ### Calculating the Final KE inside the TPC ###
+	 // ###############################################
+	 FinalKEInTPC = InitialKE - EnergyLossOutsideTPC - EnergyLossInsideTPC;
+	 hFinalKEInTPC->Fill(FinalKEInTPC);
+	 
+	 
+	 // ------------------------------------------------------------------------------------
+	 // ---------------        Extrapolate the X, Y, Z position of the primary         -----
+	 // ---------------     if it started upstream of the front face of the TPC        -----
+	 // ------------------------------------------------------------------------------------
+	 
+	 double b1 = StartPointz[iG4] - StartPointx[iG4]*Pz[iG4]/Px[iG4];
+	 double b2 = StartPointz[iG4] - StartPointy[iG4]*Pz[iG4]/Py[iG4];
+	 
+	 g4Primary_ProjX0 = -b1*Px[iG4]/Pz[iG4];
+	 g4Primary_ProjY0 = -b2*Py[iG4]/Pz[iG4];
+	 g4Primary_ProjZ0 = 0.0;
+	 
+	 hELossXvsY->Fill(g4Primary_ProjX0, g4Primary_ProjY0, EnergyLossOutsideTPC);
+	 
+	 hELossXvsYFlux->Fill(g4Primary_ProjX0, g4Primary_ProjY0);
+	 
 	 }//<---Only looking at primary particles
       
-      
-      
+      float P = sqrt( (Px[iG4]*Px[iG4]) + (Py[iG4]*Py[iG4]) + (Pz[iG4]*Pz[iG4])) * 1000;
+      //if(FinalKEInTPC == 0){std::cout<<"Run "<<run<<", SubRun "<<subrun<<", Event "<<event<<" P "<<P<<std::endl;}
       }//<---End iG4Pri loop
    
    
+   // ### Counting the events which pass through the TPC ###
+   if(EventsReachTPC){nEventsReachTPC++;}
+   if(EventsWhereParticleStops){nParticlesStop++;}
+   
    }//<---End jentry loop
+
+
+std::cout<<"========================================================================================"<<std::endl;
+std::cout<<"Total Number of Events 				"<<nTotalEvents<<std::endl;
+std::cout<<"Particles reach the TPC				"<<nEventsReachTPC<<std::endl;
+std::cout<<"Particle stops inside the TPC			"<<nParticlesStop<<std::endl;
+std::cout<<"========================================================================================"<<std::endl;
+
 
 // ########################
 // ### Making a TCanvas ###
@@ -272,5 +367,298 @@ leg->SetShadowColor(kWhite);
 leg->SetHeader("Proton MC");
 leg->Draw();
 
+
+//----------------------------------------------------------------------------------------------
+
+// ########################
+// ### Making a TCanvas ###
+// ########################
+TCanvas *c03= new TCanvas("c03","Initial KE");
+c03->SetTicks();
+c03->SetFillColor(kWhite);
+
+// ### Formatting the histograms ###
+// ### Formatting the histograms ###
+hInitialKE->SetLineColor(kBlue);
+hInitialKE->SetLineStyle(0);
+hInitialKE->SetLineWidth(3);
+hInitialKE->SetMarkerStyle(8);
+hInitialKE->SetMarkerSize(0.9);
+
+// ### Labeling the axis ###
+hInitialKE->GetXaxis()->SetTitle("K.E._{inital} (MeV)");
+hInitialKE->GetXaxis()->CenterTitle();
+
+hInitialKE->GetYaxis()->SetTitle("Events / 10 MeV");
+hInitialKE->GetYaxis()->CenterTitle();
+
+// ### Drawing the histogram ### 
+hInitialKE->Draw();
+
+// ############################
+// # Setting the Latex Header #
+// ############################
+//TLatex *t = new TLatex();
+t->SetNDC();
+t->SetTextFont(62);
+t->SetTextSize(0.04);
+t->SetTextAlign(40);
+t->DrawLatex(0.1,0.90,"LArIAT Preliminary");
+t->DrawLatex(0.13,0.84,""); 
+
+// ######################
+// # Setting the Legend #
+// ######################
+//TLegend *leg = new TLegend();
+leg = new TLegend(0.58,0.65,0.88,0.88);
+leg->SetTextSize(0.04);
+leg->SetTextAlign(12);
+leg->SetFillColor(kWhite);
+leg->SetLineColor(kWhite);
+leg->SetShadowColor(kWhite);
+leg->SetHeader("Proton MC");
+leg->Draw();
+
+
+
+//----------------------------------------------------------------------------------------------
+
+// ########################
+// ### Making a TCanvas ###
+// ########################
+TCanvas *c04= new TCanvas("c04","Initial KE at TPC");
+c04->SetTicks();
+c04->SetFillColor(kWhite);
+
+// ### Formatting the histograms ###
+hInitialKEAtTPC->SetLineColor(kBlue);
+hInitialKEAtTPC->SetLineStyle(0);
+hInitialKEAtTPC->SetLineWidth(3);
+hInitialKEAtTPC->SetMarkerStyle(8);
+hInitialKEAtTPC->SetMarkerSize(0.9);
+
+// ### Labeling the axis ###
+hInitialKEAtTPC->GetXaxis()->SetTitle("K.E._{inital} @ TPC (MeV)");
+hInitialKEAtTPC->GetXaxis()->CenterTitle();
+
+hInitialKEAtTPC->GetYaxis()->SetTitle("Events / 10 MeV");
+hInitialKEAtTPC->GetYaxis()->CenterTitle();
+
+// ### Drawing the histogram ### 
+hInitialKEAtTPC->Draw();
+
+// ############################
+// # Setting the Latex Header #
+// ############################
+//TLatex *t = new TLatex();
+t->SetNDC();
+t->SetTextFont(62);
+t->SetTextSize(0.04);
+t->SetTextAlign(40);
+t->DrawLatex(0.1,0.90,"LArIAT Preliminary");
+t->DrawLatex(0.13,0.84,""); 
+
+// ######################
+// # Setting the Legend #
+// ######################
+//TLegend *leg = new TLegend();
+leg = new TLegend(0.58,0.65,0.88,0.88);
+leg->SetTextSize(0.04);
+leg->SetTextAlign(12);
+leg->SetFillColor(kWhite);
+leg->SetLineColor(kWhite);
+leg->SetShadowColor(kWhite);
+leg->SetHeader("Proton MC");
+leg->Draw();
+
+
+//----------------------------------------------------------------------------------------------
+
+// ########################
+// ### Making a TCanvas ###
+// ########################
+TCanvas *c05= new TCanvas("c05","Final KE in TPC");
+c05->SetTicks();
+c05->SetFillColor(kWhite);
+
+// ### Formatting the histograms ###
+hFinalKEInTPC->SetLineColor(kBlue);
+hFinalKEInTPC->SetLineStyle(0);
+hFinalKEInTPC->SetLineWidth(3);
+hFinalKEInTPC->SetMarkerStyle(8);
+hFinalKEInTPC->SetMarkerSize(0.9);
+
+// ### Labeling the axis ###
+hFinalKEInTPC->GetXaxis()->SetTitle("K.E._{Final} (MeV)");
+hFinalKEInTPC->GetXaxis()->CenterTitle();
+
+hFinalKEInTPC->GetYaxis()->SetTitle("Events / 10 MeV");
+hFinalKEInTPC->GetYaxis()->CenterTitle();
+
+// ### Drawing the histogram ### 
+hFinalKEInTPC->Draw();
+
+// ############################
+// # Setting the Latex Header #
+// ############################
+//TLatex *t = new TLatex();
+t->SetNDC();
+t->SetTextFont(62);
+t->SetTextSize(0.04);
+t->SetTextAlign(40);
+t->DrawLatex(0.1,0.90,"LArIAT Preliminary");
+t->DrawLatex(0.13,0.84,""); 
+
+// ######################
+// # Setting the Legend #
+// ######################
+//TLegend *leg = new TLegend();
+leg = new TLegend(0.58,0.65,0.88,0.88);
+leg->SetTextSize(0.04);
+leg->SetTextAlign(12);
+leg->SetFillColor(kWhite);
+leg->SetLineColor(kWhite);
+leg->SetShadowColor(kWhite);
+leg->SetHeader("Proton MC");
+leg->Draw();
+
+
+
+//----------------------------------------------------------------------------------------------
+
+// ########################
+// ### Making a TCanvas ###
+// ########################
+TCanvas *c06= new TCanvas("c06","Energy Loss vs X and Y");
+c06->SetTicks();
+c06->SetFillColor(kWhite);
+
+// ### Formatting the histograms ###
+
+// ### Labeling the axis ###
+hELossXvsY->GetXaxis()->SetTitle("X Postion at TPC Face (cm)");
+hELossXvsY->GetXaxis()->CenterTitle();
+
+hELossXvsY->GetYaxis()->SetTitle("Y Position at TPC Face (cm)");
+hELossXvsY->GetYaxis()->CenterTitle();
+
+// ### Drawing the histogram ### 
+hELossXvsY->Draw("colz");
+
+// ############################
+// # Setting the Latex Header #
+// ############################
+//TLatex *t = new TLatex();
+t->SetNDC();
+t->SetTextFont(62);
+t->SetTextSize(0.04);
+t->SetTextAlign(40);
+t->DrawLatex(0.1,0.90,"LArIAT Preliminary");
+t->DrawLatex(0.13,0.84,""); 
+
+// ######################
+// # Setting the Legend #
+// ######################
+//TLegend *leg = new TLegend();
+//leg = new TLegend(0.58,0.65,0.88,0.88);
+//leg->SetTextSize(0.04);
+//leg->SetTextAlign(12);
+//leg->SetFillColor(kWhite);
+//leg->SetLineColor(kWhite);
+//leg->SetShadowColor(kWhite);
+//leg->SetHeader("Proton MC");
+//leg->Draw();
+
+//----------------------------------------------------------------------------------------------
+
+// ########################
+// ### Making a TCanvas ###
+// ########################
+TCanvas *c07= new TCanvas("c07","Energy Loss vs X and Y Flux");
+c07->SetTicks();
+c07->SetFillColor(kWhite);
+
+// ### Formatting the histograms ###
+
+// ### Labeling the axis ###
+hELossXvsYFlux->GetXaxis()->SetTitle("X Postion at TPC Face (cm)");
+hELossXvsYFlux->GetXaxis()->CenterTitle();
+
+hELossXvsYFlux->GetYaxis()->SetTitle("Y Position at TPC Face (cm)");
+hELossXvsYFlux->GetYaxis()->CenterTitle();
+
+// ### Drawing the histogram ### 
+hELossXvsYFlux->Draw("colz");
+
+// ############################
+// # Setting the Latex Header #
+// ############################
+//TLatex *t = new TLatex();
+t->SetNDC();
+t->SetTextFont(62);
+t->SetTextSize(0.04);
+t->SetTextAlign(40);
+t->DrawLatex(0.1,0.90,"LArIAT Preliminary");
+t->DrawLatex(0.13,0.84,""); 
+
+// ######################
+// # Setting the Legend #
+// ######################
+//TLegend *leg = new TLegend();
+//leg = new TLegend(0.58,0.65,0.88,0.88);
+//leg->SetTextSize(0.04);
+//leg->SetTextAlign(12);
+//leg->SetFillColor(kWhite);
+//leg->SetLineColor(kWhite);
+//leg->SetShadowColor(kWhite);
+//leg->SetHeader("Proton MC");
+//leg->Draw();
+
+//----------------------------------------------------------------------------------------------
+
+// ########################
+// ### Making a TCanvas ###
+// ########################
+TCanvas *c08= new TCanvas("c08","Energy Loss vs X and Y Flux");
+c08->SetTicks();
+c08->SetFillColor(kWhite);
+
+// ### Formatting the histograms ###
+
+hELossXvsYDivide->Divide(hELossXvsY, hELossXvsYFlux);
+
+// ### Labeling the axis ###
+hELossXvsYDivide->GetXaxis()->SetTitle("X Postion at TPC Face (cm)");
+hELossXvsYDivide->GetXaxis()->CenterTitle();
+
+hELossXvsYDivide->GetYaxis()->SetTitle("Y Position at TPC Face (cm)");
+hELossXvsYDivide->GetYaxis()->CenterTitle();
+
+// ### Drawing the histogram ### 
+hELossXvsYDivide->Draw("colz");
+
+// ############################
+// # Setting the Latex Header #
+// ############################
+//TLatex *t = new TLatex();
+t->SetNDC();
+t->SetTextFont(62);
+t->SetTextSize(0.04);
+t->SetTextAlign(40);
+t->DrawLatex(0.1,0.90,"LArIAT Preliminary");
+t->DrawLatex(0.13,0.84,""); 
+
+// ######################
+// # Setting the Legend #
+// ######################
+//TLegend *leg = new TLegend();
+//leg = new TLegend(0.58,0.65,0.88,0.88);
+//leg->SetTextSize(0.04);
+//leg->SetTextAlign(12);
+//leg->SetFillColor(kWhite);
+//leg->SetLineColor(kWhite);
+//leg->SetShadowColor(kWhite);
+//leg->SetHeader("Proton MC");
+//leg->Draw();
 
 }//<---End Loop()
